9e9588cb162b477d9d9fed3e20319d23da799e2a
Michal <m.baraniak@streamsoft.pl>
2019-02-09 18:13:40 +0100

    Initial project

---
+++ src/main/java/pl/lean/studentdictionary/StudentDictionaryApplication.java
@@ -0,0 +1,14 @@
+package pl.lean.studentdictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDictionaryApplication.class, args);
+	}
+
+}
+
